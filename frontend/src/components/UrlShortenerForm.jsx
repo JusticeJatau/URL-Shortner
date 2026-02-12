6684b37d8bc436b77a14f59e29c84e160b819f08
Justice Jatau <69175551+JusticeJatau@users.noreply.github.com>
2026-02-12 15:11:46 +0100

    Update BACKEND_URL to use VITE_BACKEND_URL

--- frontend/src/components/UrlShortenerForm.jsx
+++ frontend/src/components/UrlShortenerForm.jsx
@@ -9,7 +9,7 @@ const UrlShortenerForm = () => {
   const [copied, setCopied] = useState(false);
 
   const BASE_URL = import.meta.env.VITE_BASE_URL;
-  const BACKEND_URL = import.meta.env.BACKEND_URL;
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
   const handleSubmit = async(e) => {
     e.preventDefault();
