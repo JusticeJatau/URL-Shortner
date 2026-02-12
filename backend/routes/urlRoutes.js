dc7e18153f5de15757b9b41054db406ce41cb78a
Justice Jatau <69175551+JusticeJatau@users.noreply.github.com>
2026-02-12 13:34:05 +0100

    Fix import path for URLController

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createShortUrl, redirectToOriginalUrl, getAllUrls } from '../controller/URLController.js';
+import { createShortUrl, redirectToOriginalUrl, getAllUrls } from '../controller/urlController.js';
 
 const router = express.Router();
 // Create short URL
